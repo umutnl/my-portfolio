6405b4f31b3ea6893833d25ba54d40979d9b6c64
Umut Gok <umutnl@hotmail.com>
2022-03-30 13:23:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+/*
+function DisplayMenu(obj) {
+    var obj = document.getElementById(obj);
+            if (obj.style.visibility == 'visible') {
+                obj.style.visibility = 'hidden';
+            }
+            else {
+                obj.style.visibility = 'visible';
+            }
+        }
+
+  */
+/*
+        function myFunction() {
+            var x = document.getElementById("fname");
+            x.value = x.value.toLowerCase();
+          }
+
+          */
+
+
+
+/*The <input type="range"> defines a control for entering a number whose exact value is not important (like a slider control).
+
+Default range is 0 to 100. However, you can set restrictions on what numbers are accepted with the attributes below.
+
+max - specifies the maximum value allowed
+min - specifies the minimum value allowed
+step - specifies the legal number intervals
+value - Specifies the default value
+Tip: Always add the <label> tag for bes */
+
+/*
+A cookie header is stored in the browser as follows: 
+
+Set-Cookie: name=value; expires=date; path=path; domain=domain; secure */
